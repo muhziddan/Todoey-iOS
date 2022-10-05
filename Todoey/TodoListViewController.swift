598736ee9555e2f09860986298abac848bd4f36c
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-10-05 11:02:21 +0700

    Change dummy data to dummy array, deselect array when it is already selected, and add checkmark if cell get clicked

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
+    
+    let array = ["A", "B", "C"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,21 +20,27 @@ class TodoListViewController: UITableViewController {
     
 }
 
-//MARK: - TableView Datasource Methods
+//MARK: - TableView Datasource and Delegate Methods
 extension TodoListViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return array.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = "Test"
+        cell.textLabel?.text = array[indexPath.row]
         
         return cell
     }
     
-    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let selectedCell = tableView.cellForRow(at: indexPath)
+        selectedCell?.accessoryType == .checkmark ? (selectedCell?.accessoryType = .none) : (selectedCell?.accessoryType = .checkmark)
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
