e4f4a0cac98eab6536854ff558bbdfea57db02eb
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-10-05 09:23:10 +0700

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Muhammad Ziddan Hidayatullah on 05/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
