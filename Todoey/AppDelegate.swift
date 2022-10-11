3efa347cd2a17f7c34143a28190d860a90e37293
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-10-11 20:09:11 +0700

    Remove print for plist directory

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -15,9 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        // Get simulator and sandbox file path
-        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
-        
         return true
     }
 
